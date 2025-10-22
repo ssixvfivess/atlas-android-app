e25a74a4aee1f8cabbfe6b94dd98535af4c35ed8
ssixvfivess <ssixvfivess@yandex.ru>
2025-10-22 22:17:09 +0500

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -10,12 +13,31 @@ android {
 
     defaultConfig {
         applicationId = "com.ssixvfivess.anatomy"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    packagingOptions {
+        resources.excludes += "DebugProbesKt.bin"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 
     buildTypes {
@@ -39,21 +61,77 @@ android {
     }
 }
 
-dependencies {
+    dependencies {
+        // ANDROID LIBS
+        implementation(libs.kotlin)
+        implementation(libs.appcompat)
+        implementation(libs.coreKtx)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-}
+        implementation(libs.fragmentKtx)
+        implementation(libs.viewModelKtx)
+
+        // MATERIAL COMPONENTS
+        implementation(libs.coreKtx)
+        implementation(libs.material)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.ui)
+        implementation(libs.androidx.ui.graphics)
+        implementation(libs.androidx.ui.tooling.preview)
+        implementation(libs.androidx.material3)
+        testImplementation(libs.junit)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.ui.test.junit4)
+        debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+        implementation(libs.swipeRefreshLayout)
+        implementation(libs.constraintLayout)
+
+        // UI
+        implementation(libs.adapterDelegates)
+
+        // MEDIA
+        implementation(libs.coil.compose)
+        implementation(libs.glide)
+        implementation(libs.lifecycle.viewmodel.savedstate.android)
+        implementation(libs.protolite.well.known.types)
+        implementation(libs.room.runtime.android)
+        implementation(libs.runtime)
+        implementation(libs.material3)
+        implementation(libs.ui.tooling.preview)
+        debugImplementation(libs.ui.tooling)
+        ksp(libs.glideCompiler)
+
+        // NETWORK
+        implementation(libs.retrofit)
+        implementation(libs.okhttp3Logging)
+        implementation(libs.ktxSerializationConverter)
+
+        // NAVIGATION
+        implementation(libs.navComponentsUi)
+        implementation(libs.navComponentsFragment)
+
+        // MULTITHREADING
+        implementation(libs.coroutines)
+
+        // DI
+        implementation(libs.hilt.android)
+        ksp(libs.hilt.compiler)
+        implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+
+        // LOG
+        implementation(libs.timber)
+
+        // LOCAL STORAGE
+        implementation(libs.room)
+        ksp(libs.roomCompiler)
+
+        // SYSTEM UTILS
+        implementation(libs.jodaTime)
+        implementation(libs.ktxSerializationJson)
+
+        implementation(libs.material.v190)
+
+        //SPLASH SCREEN API
+        implementation(libs.androidx.core.splashscreen)
+    }
